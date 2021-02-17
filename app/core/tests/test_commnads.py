af99e6f41c5d3ac00fa5af8507d10970edf9fbba
nathon reed <nathon@nathons-MacBook-Pro.local>
2021-02-17 11:36:24 -0800

    able to login user register usr view user profile and update user pass 20/20 test no flake8 errors"

--- app/core/tests/test_commnads.py
+++ app/core/tests/test_commnads.py
@@ -1,7 +1,8 @@
-from unittest.mock import patch 
+from unittest.mock import patch
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
+
 
 class CommandsTestCase(TestCase):
 
